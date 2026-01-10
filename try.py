f57efac777ad3086d7d677b10b4ce818920ff613
hagar abseed <hagarsaad301@gmail.com>
2026-01-11 01:28:34 +0200

    third lecture

--- try.py
+++ try.py
@@ -6,11 +6,45 @@
 # var = int (input("guess the number: "))
 # print (secret == var)
 
-secret = 10
-var = int (input("guess the number: "))
-if secret>var:
-    print("Your guess is too low!")
-elif secret<var:
-    print("Your guess is too high!")
-else:
-    print("Your guess is the same as the secret")
+# secret = 10
+# var = int (input("guess the number: "))
+# if secret>var:
+#     print("Your guess is too low!")
+# elif secret<var:
+#     print("Your guess is too high!")
+# else:
+#     print("Your guess is the same as the secret")
+
+# counter = 0
+# where = input ("Go left or right? ")
+# while where == "right" :
+#     counter +=1
+#     if counter >= 2:
+#         print(":(")
+#     where = input ("Go left or right? ")
+# print ("You got out!")
+
+# mysum = 0
+# start = 3
+# end = 5
+# for i in range (start,end+1):
+#     print("i= ",i)
+#     mysum += i
+# print("The sum = ",mysum)
+
+# factorial code
+# using while loop:
+x = 4
+i = 1
+fact = 1
+while i <= x :
+    fact *= i
+    i += 1
+print(x,"Factorial is",fact)
+
+# using for loop :
+# x = 4
+# fact = 1
+# for i in range (1, x+1) :
+#     fact *= i
+# print(x,"Factorial is",fact)   
